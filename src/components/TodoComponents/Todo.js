0e777f0133a19a71d8511db81312cbbe3369f1ba
cladams <cladams0203@gmail.com>
2019-06-12 14:52:46 -0400

    created Todo functional component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Todo() {
+  return(
+    <div>
+      <h4>{this.state.todo}</h4>
+    </div>
+  )
+}
+
+export default Todo;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -7,3 +7,5 @@ function TodoList() {
     </form>
   )
 }
+
+export default TodoList;
